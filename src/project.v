58d2dafc5f5a684b7373bb43e946b1366d87d70e
Hridi Prova Debnath <111787740+Hridi1992@users.noreply.github.com>
2024-07-03 11:58:13 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,14 +6,14 @@
 `default_nettype none
 
 module tt_um_example (
-    input logic [7:0] ui_in,    // Dedicated inputs
-    output logic [7:0] uo_out,   // Dedicated outputs
-    input logic [7:0] uio_in,   // IOs: Input path
-    output logic [7:0] uio_out,  // IOs: Output path
-    output logic [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  logic       ena,      // always 1 when the design is powered, so you can ignore it
-    input  logic       clk,      // clock
-    input  logic      rst_n     // reset_n - low to reset
+    input wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire      rst_n     // reset_n - low to reset
 );
 
   // All output pins must be assigned. If not used, assign to 0.
